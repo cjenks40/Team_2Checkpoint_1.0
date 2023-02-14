d5b96c99386ea64521f4a9eeb74b43d6145e3684
SethCottengim <cottengimseth@icloud.com>
2023-02-13 22:32:45 -0800

    Week 4 assignment revision - getData()

---
+++ ACS.R
@@ -0,0 +1,4 @@
+# Read in the three .csv files
+cities <- read.csv("project.acs.cities.csv")
+counties <- read.csv("project.acs.counties.csv")
+zips <- read.csv("project.acs.zipcodes.csv")
---
+++ Sales.R
@@ -0,0 +1,5 @@
+# Read in the three .csv files
+cities <- read.csv("project.sales.cities.csv")
+counties <- read.csv("project.sales.counties.csv")
+zips <- read.csv("project.sales.zipcodes.csv")
+
